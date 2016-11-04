6bfd5840c1402c0ce2c7f0bd31b9c7b71a95c959
Emil Edholm <bobby@verypublic.org>
2016-11-04 16:34:13 +0100

    Add test for frame.getContent()

--- src/test/java/messages/FrameTest.java
+++ src/test/java/messages/FrameTest.java
@@ -2,6 +2,9 @@ package messages;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -10,6 +13,18 @@ import static org.junit.Assert.*;
 public class FrameTest {
     @Test
     public void getContent() throws Exception {
-        fail("Not implemented yet");
+        Frame f1 = new Frame.Builder(0xFFFF ).tagged(true).source(0xFFFFFFFFL).build();
+        Frame f2 = new Frame.Builder(0xFFFF ).tagged(false).source(0xFFFFFFFFL).build();
+
+        byte ff = (byte) 0xff;
+        byte[] f1ExpectedContent = new byte[] {
+                ff, ff, 0x0, 0x34, ff, ff, ff, ff
+        };
+        byte[] f2ExpectedContent = new byte[] {
+            ff, ff, 0x0, 0x14, ff, ff, ff, ff
+        };
+
+        assertThat(f1.getContent(), is(f1ExpectedContent));
+        assertThat(f2.getContent(), is(f2ExpectedContent));
     }
 }
