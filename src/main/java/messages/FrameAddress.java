6cf876ad1866ac1af470d0cfceda26a4991a9642
Emil Edholm <bobby@verypublic.org>
2016-11-04 19:57:46 +0100

    Add frame address message

--- src/main/java/messages/FrameAddress.java
+++ src/main/java/messages/FrameAddress.java
@@ -1,7 +1,91 @@
 package messages;
 
+import utils.ByteUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 /**
+ * The Frame Address section contains the following routing information:
+ * Target device address
+ * Acknowledgement message is required flag
+ * State response message is required flag
+ * Message sequence number
+ * <p>
+ * The FrameAddress is 16 byte long (128 bits)
  * Created by Emil Edholm on 2016-11-04.
  */
-public class FrameAddress {
+public final class FrameAddress {
+    private static final int SIZE = 16;
+    public static final int ACK_REQUIRED_POSITION = 9;
+    public static final int RES_REQUIRED_POSITION = 8;
+    private final long target;
+    private final int ackRequired;
+    private final int resRequired;
+    private final byte sequence;
+
+    private final byte[] content;
+
+    public static final class Builder {
+        private long target = 0x0;
+        private int ackRequired = 0x0;
+        private int resRequired = 0x0;
+        private byte sequence = 0x0;
+
+        public Builder target(long target) {
+            this.target = target;
+            return this;
+        }
+
+        public Builder ackRequired(boolean ackRequired) {
+            this.ackRequired = (ackRequired) ? 1 << ACK_REQUIRED_POSITION : 0;
+            return this;
+        }
+
+        public Builder resRequired(boolean resRequired) {
+            this.resRequired = (resRequired) ? 1 << RES_REQUIRED_POSITION : 0;
+            return this;
+        }
+
+        public Builder sequence(int sequence) {
+            if (sequence < 0 || sequence > 0xFF) {
+                throw new IllegalArgumentException("Sequence larger than is possible. Got: " + sequence);
+            }
+            this.sequence = (byte) sequence;
+            return this;
+        }
+
+        public FrameAddress build() {
+            return new FrameAddress(this);
+        }
+    }
+
+    private FrameAddress(Builder b) {
+        this.target = b.target;
+        this.ackRequired = b.ackRequired;
+        this.resRequired = b.resRequired;
+        this.sequence = b.sequence;
+
+        ByteBuffer bb = ByteBuffer.allocate(SIZE);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.putLong(target);
+        // & with 0xff to get unsigned value
+        bb.putLong(ackRequired | resRequired | (sequence & 0xff));
+        this.content = bb.array();
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return "FrameAddress{" +
+                "target=" + String.format("0x%x", target) +
+                ", ackRequired=" + ((ackRequired > 0) ? "true" : "false") +
+                ", resRequired=" + ((resRequired > 0) ? "true" : "false") +
+                ", sequence=" + String.format("0x%x", sequence) +
+                ", content=" + ByteUtils.toHexString(content) +
+                '}';
+    }
 }
--- src/test/java/messages/FrameAddressTest.java
+++ src/test/java/messages/FrameAddressTest.java
@@ -1,8 +1,44 @@
-import static org.junit.Assert.*;
+package messages;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Emil Edholm on 2016-11-04.
  */
 public class FrameAddressTest {
+    @Test
+    public void correctSize() throws Exception {
+        FrameAddress fa = new FrameAddress.Builder().build();
+        assertThat(fa.getContent().length, is(16));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sequenceToLarge() throws Exception {
+        new FrameAddress.Builder().sequence(0x100).build();
+    }
+
+    @Test
+    public void getContent() throws Exception {
+        FrameAddress fa1 = new FrameAddress.Builder().build();
+        FrameAddress fa2 = new FrameAddress.Builder().ackRequired(true).resRequired(true).target(0xFFFFFFFFFFFFFFFFL).sequence(0xff).build();
+
+        System.out.println("FA1: " + fa1);
+        System.out.println("FA2: " + fa2);
+        byte[] fa1Expected = new byte[]{
+                0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
+        };
+        
+        byte ff = (byte) 0xff;
+        byte[] fa2Expected = new byte[]{
+                ff, ff, ff, ff, ff, ff, ff, ff, ff, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        };
+
+        assertThat(fa1.getContent(), is(fa1Expected));
+        assertThat(fa2.getContent(), is(fa2Expected));
+    }
 
 }
