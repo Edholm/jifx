b5f638bb777bf220c8d0101d7bde939badcc6627
Emil Edholm <bobby@verypublic.org>
2016-11-05 12:04:50 +0100

    Private constructor

--- src/main/java/pub/edholm/jifx/utils/Constants.java
+++ src/main/java/pub/edholm/jifx/utils/Constants.java
@@ -6,6 +6,7 @@ import java.nio.ByteOrder;
  * Created by Emil Edholm on 2016-11-04.
  */
 public class Constants {
+    private Constants() {}
     public static final int SIZE_HSBK = 8;
     public static final int SIZE_PROTOCOL_HEADER = 12;
     public static final int SIZE_FRAME_ADDRESS = 16;
