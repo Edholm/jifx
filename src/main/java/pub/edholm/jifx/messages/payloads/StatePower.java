3e966d8981eba94c94d8d8043491b3e76f0d8ef0
Emil Edholm <bobby@verypublic.org>
2016-11-06 21:39:09 +0100

    Add StatePower payload

---
+++ src/main/java/pub/edholm/jifx/messages/payloads/StatePower.java
@@ -0,0 +1,58 @@
+package pub.edholm.jifx.messages.payloads;
+
+import pub.edholm.jifx.messages.Message;
+import pub.edholm.jifx.messages.datatypes.PowerLevel;
+import pub.edholm.jifx.utils.Constants;
+
+/**
+ * Created by Emil Edholm on 2016-11-06.
+ */
+public class StatePower implements Message {
+    private final PowerLevel level;
+
+    public StatePower(PowerLevel level) {
+        this.level = level;
+    }
+
+    public static StatePower valueOf(byte[] content) {
+        final PowerLevel level = PowerLevel.valueOf(content);
+        return new StatePower(level);
+    }
+
+    public PowerLevel getLevel() {
+        return level;
+    }
+
+    @Override
+    public int size() {
+        return Constants.SIZE_POWER_LEVEL;
+    }
+
+    @Override
+    public byte[] getContent() {
+        return level.getContent();
+    }
+
+    @Override
+    public String toString() {
+        return "StatePower{" +
+                "level=" + level +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StatePower that = (StatePower) o;
+
+        return level.equals(that.level);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return level.hashCode();
+    }
+}
