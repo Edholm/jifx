f625bd629ce501d8390b80a77706c7630ed0ab69
Emil Edholm <bobby@verypublic.org>
2016-11-26 12:04:56 +0100

    Add more convenience methods

--- src/main/java/pub/edholm/jifx/utils/ByteUtils.java
+++ src/main/java/pub/edholm/jifx/utils/ByteUtils.java
@@ -1,15 +1,13 @@
 package pub.edholm.jifx.utils;
 
-import pub.edholm.jifx.messages.MessagePart;
-
 import java.nio.ByteBuffer;
-import java.util.List;
 
 /**
  * Created by Emil Edholm on 2016-11-01.
  */
 public final class ByteUtils {
-    private static final String HEX_FORMAT = "%02x ";
+    private static final String HEX_FORMAT = "%02x";
+    private static final String SEPARATOR = " ";
 
     private ByteUtils() {
         throw new AssertionError("You mustn't create an instance of this class");
@@ -19,7 +17,7 @@ public final class ByteUtils {
         StringBuilder sb = new StringBuilder();
         sb.append("0x");
         for (byte b : array) {
-            sb.append(String.format(HEX_FORMAT, b));
+            sb.append(String.format(HEX_FORMAT + SEPARATOR, b));
         }
 
         return sb.toString();
@@ -27,5 +25,27 @@ public final class ByteUtils {
 
     public static <T> String toHexString(T s) {
         return "0x" + String.format(HEX_FORMAT, s);
+    }
+
+    public static String toMacAddressString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            sb.append(String.format(HEX_FORMAT, b));
+            if (i < bytes.length - 1) {
+                sb.append(':');
+            }
+        }
+        return sb.toString();
+    }
+
+    public static byte[] emptyByteArray(int size) {
+        return new byte[size];
+    }
+
+    public static byte[] longToBytes(long l) {
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+        buffer.putLong(l);
+        return buffer.array();
     }
 }
