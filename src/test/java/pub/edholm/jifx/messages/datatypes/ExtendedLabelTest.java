56bd5c48485ff98eb96572d45927cf0499cb6f6f
Emil Edholm <bobby@verypublic.org>
2016-11-26 18:01:57 +0100

    Add ExtendedLabel data type

---
+++ src/test/java/pub/edholm/jifx/messages/datatypes/ExtendedLabelTest.java
@@ -0,0 +1,20 @@
+package pub.edholm.jifx.messages.datatypes;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by Emil Edholm on 2016-11-26.
+ */
+public class ExtendedLabelTest {
+    @Test
+    public void valueOf() throws Exception {
+        final byte[] id = new byte[]{0xa, 0xb, 0xc};
+        final ExtendedLabel el = new ExtendedLabel(id, Label.valueOf("derp herp"), new Time(1480178158000000L));
+        final ExtendedLabel parsed = ExtendedLabel.valueOf(el.getContent());
+        assertThat(parsed, is(el));
+    }
+
+}
