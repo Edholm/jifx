89ffe23dce143f3a901d6ce4739406958962bb67
Emil Edholm <bobby@verypublic.org>
2016-11-27 18:22:44 +0100

    Detailed StateVersion, fixes #6
    Parses a json file with all Lifx products that is read from
    a git submodule.

--- library/src/main/java/pub/edholm/jifx/library/payloads/StateVersion.java
+++ library/src/main/java/pub/edholm/jifx/library/payloads/StateVersion.java
@@ -1,20 +1,30 @@
 package pub.edholm.jifx.library.payloads;
 
-import pub.edholm.jifx.library.exceptions.MalformedMessageException;
+import com.fasterxml.jackson.jr.ob.JSON;
+import pub.edholm.jifx.library.AbstractBuilder;
 import pub.edholm.jifx.library.AbstractMessage;
 import pub.edholm.jifx.library.MessageType;
+import pub.edholm.jifx.library.exceptions.MalformedMessageException;
 import pub.edholm.jifx.library.headers.Header;
 import pub.edholm.jifx.library.utils.ByteUtils;
-import pub.edholm.jifx.library.AbstractBuilder;
 import pub.edholm.jifx.library.utils.Constants;
 
+import java.io.IOException;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Emil Edholm on 2016-11-27.
  */
 public class StateVersion extends AbstractMessage {
+    private final static URL productsUrl = StateVersion.class.getResource("/products/products.json");
+    private List<Object> products;
+    private Map<String, Object> productMap, vendorMap;
+
     private final int vendor, product, version;
 
     private StateVersion(Header header, byte[] payloadContent, Builder b) {
@@ -25,7 +35,7 @@ public class StateVersion extends AbstractMessage {
     }
 
     public static class Builder extends AbstractBuilder<StateVersion, Builder> {
-        private int vendor, product, version;
+        private int vendor = 1, product, version;
 
         public Builder() {
             super(MessageType.StateVersion, Constants.SIZE_STATE_VERSION);
@@ -78,12 +88,74 @@ public class StateVersion extends AbstractMessage {
         return new StateVersion(h, payload, b);
     }
 
+    private void initializeProducts() {
+        if (products == null) {
+            try {
+                products = JSON.std.listFrom(productsUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+                products = Collections.emptyList();
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> findVendor(int vendorId) {
+        initializeProducts();
+        for (Object vendorObj : products) {
+            Map<String, Object> vendor = ((Map<String, Object>) vendorObj);
+            int vid = ((int) vendor.get("vid"));
+            if (vid == vendorId) {
+                return vendor;
+            }
+        }
+        return Collections.emptyMap();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> findProduct(int productId) {
+        vendorMap = findVendor(vendor);
+        List<Object> products = (List<Object>) vendorMap.getOrDefault("products", Collections.emptyList());
+
+        for (Object prodObj : products) {
+            Map<String, Object> product = (Map<String, Object>) prodObj;
+            int pid = (int) product.get("pid");
+            if (productId == pid) {
+                return product;
+            }
+        }
+
+        return Collections.emptyMap();
+    }
+
+    public String getVendorName() {
+        if (vendorMap == null) {
+            vendorMap = findVendor(vendor);
+        }
+        return (String) vendorMap.getOrDefault("name", "UNKNOWN");
+    }
+
     public int getVendor() {
         return vendor;
     }
 
     public int getProduct() {
         return product;
+    }
+
+    public String getProductName() {
+        if (productMap == null) {
+            productMap = findProduct(product);
+        }
+        return (String) productMap.getOrDefault("name", "UNKNOWN");
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Boolean> getFeatures() {
+        if (productMap == null) {
+            productMap = findProduct(product);
+        }
+        return (Map<String, Boolean>) productMap.getOrDefault("features", Collections.emptyMap());
     }
 
     public int getVersion() {
@@ -93,9 +165,10 @@ public class StateVersion extends AbstractMessage {
     @Override
     public String toString() {
         return "StateVersion{" +
-                "vendor: " + Integer.toUnsignedString(vendor) + " (" + ByteUtils.toHexString(vendor) + ")" +
-                ", product: " + Integer.toUnsignedString(product) + " (" + ByteUtils.toHexString(product) + ")" +
-                ", version: " + Integer.toUnsignedString(version) + " (" + ByteUtils.toHexString(version) + ")" +
+                "vendor: " + Integer.toUnsignedString(vendor) + " (" + getVendorName() + ")" +
+                ", product: " + Integer.toUnsignedString(product) + " (" + getProductName() + ")" +
+                ", version: " + Integer.toUnsignedString(version) +
+                ", features: " + getFeatures().toString() +
                 '}';
     }
 }
--- library/src/test/java/messages/payloads/StateVersionTest.java
+++ library/src/test/java/messages/payloads/StateVersionTest.java
@@ -3,8 +3,11 @@ package messages.payloads;
 import org.junit.Test;
 import pub.edholm.jifx.library.payloads.StateVersion;
 
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by Emil Edholm on 2016-11-27.
@@ -12,7 +15,7 @@ import static org.hamcrest.core.Is.is;
 public class StateVersionTest {
     @Test
     public void valueOf() throws Exception {
-        final StateVersion sv = new StateVersion.Builder().product(1).vendor(2).version(0xffffffff).build();
+        final StateVersion sv = new StateVersion.Builder().product(31).vendor(1).version(0xffffffff).build();
         final StateVersion parsed = StateVersion.valueOf(sv.getContent());
 
         System.out.println(sv);
@@ -21,4 +24,55 @@ public class StateVersionTest {
         assertThat(parsed, is(sv));
     }
 
+    @Test
+    public void testProductName() throws Exception {
+        final StateVersion sv = new StateVersion.Builder().product(11).build();
+        assertThat(sv.getProductName(), is("White 800 (High Voltage)"));
+
+        final StateVersion sv2 = new StateVersion.Builder().product(121).build();
+        assertThat(sv2.getProductName(), is("UNKNOWN"));
+    }
+
+    @Test
+    public void testVendorName() throws Exception {
+        final StateVersion sv = new StateVersion.Builder().vendor(1).build();
+        assertThat(sv.getVendorName(), is("LIFX"));
+
+        final StateVersion sv2 = new StateVersion.Builder().vendor(1337).build();
+        assertThat(sv2.getVendorName(), is("UNKNOWN"));
+    }
+
+    @Test
+    public void testFeatures() throws Exception {
+        final StateVersion sv = new StateVersion.Builder().product(22).build();
+        assertThat(sv.getProductName(), is("Color 1000"));
+
+        Map<String, Boolean> features = sv.getFeatures();
+        assertThat(features.get("color"), is(true));
+        assertThat(features.get("infrared"), is(false));
+        assertThat(features.get("multizone"), is(false));
+
+        final StateVersion sv2 = new StateVersion.Builder().product(30).build();
+        assertThat(sv2.getProductName(), is("LIFX+ BR30"));
+
+        Map<String, Boolean> features2 = sv2.getFeatures();
+        assertThat(features2.get("color"), is(true));
+        assertThat(features2.get("infrared"), is(true));
+        assertThat(features2.get("multizone"), is(false));
+    }
+
+    @Test
+    public void unknownFeatures() throws Exception {
+        final StateVersion sv = new StateVersion.Builder().product(111).build();
+        assertNotNull(sv.getFeatures());
+        assertThat(sv.getFeatures().size(), is(0));
+    }
+
+    @Test
+    public void testLatestProduct() throws Exception {
+        // Product 31 is the latest at the time of writing this.
+        final StateVersion sv = new StateVersion.Builder().product(32).build();
+        assertThat(sv.getProductName(), is("UNKNOWN"));
+
+    }
 }
